71959ac94f720cd24e3c32d56b841c4c5742d29e
“janvi” <jthumar1611@gmail.com>
2021-11-24 08:57:27 +0530

    first commit

---
+++ estate/models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import models, fields
+
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Estate Property Type'
+
+    name = fields.Char()
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char(string="Property Name", default="Unknown", required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=lambda self: fields.Datetime.now(), copy=False)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('north', 'North'),
+                                           ('south', 'South'),
+                                           ('east', 'East'),
+                                           ('west', 'West'), ])
+    active = fields.Boolean(default=True)
+    image = fields.Image()
+    property_type_id = fields.Many2one('estate.property.type')
---
+++ library/models/books.py
@@ -0,0 +1,84 @@
+from odoo import models, fields
+
+
+class Author(models.Model):
+    _name = 'author'
+    _description = 'Author'
+
+    name = fields.Char()
+    address = fields.Text()
+    date = fields.Date()
+
+
+# book_id = fields.Many2one('books')
+
+class BookCategory(models.Model):
+    _name = 'books.category'
+    _description = 'Book Category'
+
+    name = fields.Char()
+
+
+class BookDepartment(models.Model):
+    _name = 'books.department'
+    _description = 'Book Department'
+
+    name = fields.Char()
+
+
+class BookPublisher(models.Model):
+    _name = 'books.publisher'
+    _description = 'Book Publisher'
+
+    name = fields.Char()
+
+
+class Shelf(models.Model):
+    _name = 'shelf'
+    _description = 'Shelf'
+
+    name = fields.Char()
+    rack_id = fields.Many2one('rack')
+
+
+class Rack(models.Model):
+    _name = 'rack'
+    _description = 'Rack'
+
+    name = fields.Char()
+    shelf_ids = fields.One2many('shelf', 'rack_id')
+
+
+class LibraryDemo(models.Model):
+    _name = 'books'
+    _description = 'Library data'
+    _sql_constraints = [('isbn_unique', 'unique(isbn)', 'Duplicate isbn not allowed')]
+
+    name = fields.Char(string="Book Name", default="Book", required=True)
+    book_description = fields.Text()
+    price = fields.Float()
+    # author_ids = fields.One2many('author', 'book_id')
+    author_ids = fields.Many2many('author')
+    isbn = fields.Integer()
+    category_id = fields.Many2one('books.category')
+    department_id = fields.Many2one('books.department')
+    barcode = fields.Char()
+    publisher_id = fields.Many2one('books.publisher')
+    edition = fields.Char()  # Many 2 one
+    date = fields.Date()
+    shelf_id = fields.Many2one('shelf')
+    
+    active = fields.Boolean(default=True)
+
+#class BookIssue(models.Model):
+   # _name = 'books.issue'
+    #_description = 'Book issued'
+
+    #name = fields.Char(default="user" , required=True)
+    #email = fields.Char()
+    #address = fields.Text()
+    #issue_date = fields.Date()
+    #return_date = fields.Date()
+    #dept_ids = fields.Many2one('books.department','department_id')
+    #cat_ids = fields.Many2one('books.category','category_id')
+    #image = fields.Image()
